a0e39abad089aaaca8326a8c4289928559a38c35
shashik116 <shashik116@github.com>
2026-06-13 21:31:02 +0530

    chore: track Cargo.lock, gitignore dist/ build output

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { InferSchemaReturn, InferSchemaOptions } from "./types";
-export type { InferSchemaReturn, InferSchemaOptions, SchemaResult, SchemaError, SchemaErrorKind, ColumnMeta, InferredType, } from "./types";
-export { isSchemaError, isSchemaResult } from "./types";
-export declare function inferSchema(input: unknown, options?: InferSchemaOptions): Promise<InferSchemaReturn>;
-//# sourceMappingURL=index.d.ts.map
